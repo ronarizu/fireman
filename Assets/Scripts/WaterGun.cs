1272de5f4c3ffa8a2b4c2dcf93a2a0e62495773b
Ronny Arias <ronarizu@gmail.com>
2021-03-04 20:45:53 -0600

    FEAT: add player controller using cinemachine example

---
+++ Assets/Scripts/WaterGun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterGun : MonoBehaviour
+{
+    private ParticleSystem particleSystem;
+
+    void Start()
+    {
+        particleSystem = GetComponentInChildren<ParticleSystem>();
+        particleSystem.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            particleSystem.Play();
+            Debug.Log("dispara");
+        }
+        else
+        {
+            particleSystem.Stop();
+        }
+    }
+}
