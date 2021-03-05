f2d4a0596c86277becdcceda77d01fb6bc2fd19e
Ronny Arias <ronarizu@gmail.com>
2021-03-05 11:46:36 -0600

    remove the damping of the camera

--- Assets/Player/Scripts/CharacterAiming.cs
+++ Assets/Player/Scripts/CharacterAiming.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class CharacterAiming : MonoBehaviour
 {
+    public float turnSpeed = 15;
+    Camera mainCamera;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnSpeed * Time.fixedDeltaTime);
     }
 }
