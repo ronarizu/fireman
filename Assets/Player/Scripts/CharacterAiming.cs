665f2910bf771c0eab04773662af1cc4038d8eaa
Ronny Arias <ronarizu@gmail.com>
2021-03-05 19:39:53 -0600

    have the multiple animation

--- Assets/Player/Scripts/CharacterAiming.cs
+++ Assets/Player/Scripts/CharacterAiming.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class CharacterAiming : MonoBehaviour
 {
     public float turnSpeed = 15;
+    public float aimDuration = 0.3f;
+    public Rig aimLayer;
     Camera mainCamera;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +24,15 @@ public class CharacterAiming : MonoBehaviour
     {
         float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnSpeed * Time.fixedDeltaTime);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            aimLayer.weight += Time.deltaTime / aimDuration;
+        }else{
+            aimLayer.weight -= Time.deltaTime / aimDuration;
+        }
     }
 }
